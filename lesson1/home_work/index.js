c5ba7310f26427ca36a1a9d00429f219dcb57f55
NikitaStaroverov <starovierov98@mail.ru>
2022-02-20 22:11:15 +0300

    домашняя работа

--- lesson1/home_work/index.js
+++ lesson1/home_work/index.js
@@ -1,1 +1,15 @@
-console.log("hello script")
+const nameOurPlanet="Земля";
+let userCurrentTime=new Date();
+let numberArticles=11;
+let isPaymentCash=true;
+let surname="Староверов";
+let firstName="Никита";
+let middleName="Сергеевич";
+console.log(nameOurPlanet);
+console.log(userCurrentTime);
+console.log(numberArticles);
+console.log(isPaymentCash);
+console.log(surname);
+console.log(firstName);
+console.log(middleName);
+
