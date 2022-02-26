c3417b824e7ae3ffe6a529e52b42d0945ac4bc1f
NikitaStaroverov <starovierov98@mail.ru>
2022-02-26 11:22:35 +0300

    типы данных, операторы присвоения и сравнения

---
+++ lesson2/class_work/index.js
@@ -0,0 +1,22 @@
+// приметивные типы данных:
+//Number
+// console.log(1/0);
+// console.log(typeof NaN);
+
+//String
+
+const myString = 'строка';
+
+const anoherString = "Строка";
+
+const tempLateString = `шаблонная строка ${tempLateString}`;
+
+console.log(tempLateString);
+
+//Boolean
+
+const isAdmin = true;
+const isUser = false;
+
+//Null
+const emptyVariabel = null;
